eddcb2402dc230faf84405d44a3a3ad0a20c03ec
Electroblob77 <35599699+Electroblob77@users.noreply.github.com>
2019-12-16 12:14:01 +0000

    Don't use client-side-only vector-based AxisAlignedBB constructor, fixes #306

--- src/main/java/electroblob/wizardry/entity/construct/EntityIceSpike.java
+++ src/main/java/electroblob/wizardry/entity/construct/EntityIceSpike.java
@@ -29,9 +29,9 @@ public class EntityIceSpike extends EntityMagicConstruct {
 		this.setRotation(-facing.getHorizontalAngle(), WizardryUtilities.getPitch(facing));
 		float yaw = (-facing.getHorizontalAngle()) * (float)Math.PI/180;
 		float pitch = (WizardryUtilities.getPitch(facing) - 90) * (float)Math.PI/180;
-		Vec3d min = new Vec3d(-width/2, 0, -width/2).rotatePitch(pitch).rotateYaw(yaw);
-		Vec3d max = new Vec3d(width/2, height, width/2).rotatePitch(pitch).rotateYaw(yaw);
-		this.setEntityBoundingBox(new AxisAlignedBB(this.getPositionVector().add(min), this.getPositionVector().add(max)));
+		Vec3d min = this.getPositionVector().add(new Vec3d(-width/2, 0, -width/2).rotatePitch(pitch).rotateYaw(yaw));
+		Vec3d max = this.getPositionVector().add(new Vec3d(width/2, height, width/2).rotatePitch(pitch).rotateYaw(yaw));
+		this.setEntityBoundingBox(min.x, min.y, min.z, max.x, max.y, max.z);
 	}
 
 	public EnumFacing getFacing(){
