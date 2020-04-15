50ca70b0db978cd325f7553a9fd92ebe5178c9e9
Electroblob77 <35599699+Electroblob77@users.noreply.github.com>
2020-04-15 23:23:31 +0100

    Specify JEI and Baubles as dependencies

--- src/main/java/electroblob/wizardry/Wizardry.java
+++ src/main/java/electroblob/wizardry/Wizardry.java
@@ -46,7 +46,8 @@ import java.util.Calendar;
  */
 
 @Mod(modid = Wizardry.MODID, name = Wizardry.NAME, version = Wizardry.VERSION, acceptedMinecraftVersions = "[1.12.2]",
-		guiFactory = "electroblob.wizardry.WizardryGuiFactory", dependencies = "required-after:forge@[14.23.5.2814,);after:antiqueatlas@[4.6,)")
+		guiFactory = "electroblob.wizardry.WizardryGuiFactory",
+		dependencies = "required-after:forge@[14.23.5.2814,);after:jei@[4.15.0,);after:baubles@[1.5.2,);after:antiqueatlas@[4.6,)")
 
 public class Wizardry {
 
