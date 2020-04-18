ed63704184b1316063f779118fbfd40fc186c401
Electroblob77 <35599699+Electroblob77@users.noreply.github.com>
2020-04-18 21:10:02 +0100

    Add descriptions to mana flasks

--- src/main/java/electroblob/wizardry/item/ItemManaFlask.java
+++ src/main/java/electroblob/wizardry/item/ItemManaFlask.java
@@ -1,9 +1,17 @@
 package electroblob.wizardry.item;
 
+import electroblob.wizardry.Wizardry;
 import electroblob.wizardry.registry.WizardryTabs;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemManaFlask extends Item {
 
@@ -34,4 +42,11 @@ public class ItemManaFlask extends Item {
 	public EnumRarity getRarity(ItemStack stack){
 		return size.rarity;
 	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn){
+		Wizardry.proxy.addMultiLineDescription(tooltip, "item." + Wizardry.MODID + "mana_flask.desc", size.capacity);
+	}
+
 }
