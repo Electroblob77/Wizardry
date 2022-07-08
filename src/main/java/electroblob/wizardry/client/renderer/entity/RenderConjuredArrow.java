27e40e2069000f8d30571a2f7c4cd6e7c89fdfda
WinDanesz <31292708+WinDanesz@users.noreply.github.com>
2022-07-08 21:37:55 +0200

    fix: Fixed the opacity of conjured arrows

--- src/main/java/electroblob/wizardry/client/renderer/entity/RenderConjuredArrow.java
+++ src/main/java/electroblob/wizardry/client/renderer/entity/RenderConjuredArrow.java
@@ -2,11 +2,13 @@ package electroblob.wizardry.client.renderer.entity;
 
 import electroblob.wizardry.Wizardry;
 import electroblob.wizardry.entity.projectile.EntityConjuredArrow;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderArrow;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.opengl.GL11;
 
 @SideOnly(Side.CLIENT)
 public class RenderConjuredArrow extends RenderArrow<EntityConjuredArrow> {
@@ -21,5 +23,15 @@ public class RenderConjuredArrow extends RenderArrow<EntityConjuredArrow> {
 	@Override
 	protected ResourceLocation getEntityTexture(EntityConjuredArrow entity) {
 		return CONJURED_ARROW_TEXTURE;
+	}
+
+	@Override
+	public void doRender(EntityConjuredArrow entity, double x, double y, double z, float entityYaw, float partialTicks) {
+		GlStateManager.pushMatrix();
+		GlStateManager.enableBlend();
+		GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GlStateManager.color(1, 1, 1, 0.4f);
+		super.doRender(entity, x, y, z, entityYaw, partialTicks);
+		GlStateManager.popMatrix();
 	}
 }
